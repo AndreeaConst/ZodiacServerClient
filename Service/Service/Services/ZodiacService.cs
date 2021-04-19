a80db90f90312f3dc3699f74a62a79e7eb56b4a0
Andreea00 <andreea.constantinescu2000@outlook.com>
2021-04-20 01:18:05 +0300

    Read zodiac names and start/end dates from file to match them with the given date

--- Service/Service/Services/ZodiacService.cs
+++ Service/Service/Services/ZodiacService.cs
@@ -1,15 +1,17 @@
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using Server.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ZodiacService;
+using ZodiacServer;
 
 namespace ZodiacService
 {
     public class ZodiacService : Zodiac.ZodiacBase
     {
+        private readonly ZodiacOperations zodiacOperations = new ZodiacOperations();
         private readonly ILogger<ZodiacService> _logger;
         public ZodiacService(ILogger<ZodiacService> logger)
         {
@@ -18,10 +20,12 @@ namespace ZodiacService
 
         public override Task<ZodiacReply> FindZodiac(ZodiacRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new ZodiacReply
-            {
-                
-            });
+            var zodiac = request.Zodiac;
+            string zodiacName = zodiacOperations.CalculateZodiac(zodiac);
+
+            _logger.Log(LogLevel.Information, "Calculated zodiac");
+
+            return Task.FromResult(new ZodiacReply() { ZodiacName =  zodiacName});
         }
     }
 }
